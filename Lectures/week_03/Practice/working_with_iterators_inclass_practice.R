1bf8b2399cfd97fe40a8d7aa9b51cbefb3f39e92
edunford <eric.dunford@georgetown.edu>
2019-09-18 18:07:44 -0400

    Adding materials for week 3 lecture

---
+++ Lectures/week_03/Practice/working_with_iterators_inclass_practice.R
@@ -0,0 +1,46 @@
+#'
+#' Practice: Working with Iterators
+#'
+
+
+#'
+#' Question 1:
+#'
+#' Write a loop that iterates over the following two vectors, and multiplies the
+#' values located at each index location.
+#'
+
+vec1 = c(3,5,7,3,99,-10)
+vec2 = c(3.3,4.2,77.5,6,102,10034)
+
+
+
+
+
+#'
+#' Question 2:
+#'
+#' Write a loop that iterate through the `mtcars` data frame and print the
+#' message "Low Gear" if the gear variable is less than 4, else print "High
+#' Gear"
+#'
+
+dat <- mtcars
+
+
+
+
+#'
+#' Question 3
+#'
+#' Write a while loop that multiplies the current and prior values in the vector
+#' `values`. Make sure the while loop stops iterating after the length of the
+#' added number exceeds 6 digits (hint: `nchar(100)`)
+#'
+
+values = 1:10e7
+
+
+
+
+
